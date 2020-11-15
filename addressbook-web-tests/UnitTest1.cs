d782364694a7a41a49048daff03ab145e430e7ee
1 <zhuzepa@yandex.ru>
2020-11-16 00:35:57 +0300

    Revert "Изменил на 3"
    This reverts commit 24f5e6419fbbf3f808bd0f7537fa33424e3af86e.

--- addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/UnitTest1.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace addressbook_web_tests
 {
 	[TestClass]
-	public class UnitTest3
+	public class UnitTest1
 	{
 		[TestMethod]
 		public void TestMethod2()
