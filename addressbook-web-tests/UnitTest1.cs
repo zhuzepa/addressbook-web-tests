24f5e6419fbbf3f808bd0f7537fa33424e3af86e
1 <zhuzepa@yandex.ru>
2020-11-16 00:34:18 +0300

    Изменил на 3

--- addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/UnitTest1.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace addressbook_web_tests
 {
 	[TestClass]
-	public class UnitTest1
+	public class UnitTest3
 	{
 		[TestMethod]
 		public void TestMethod2()
